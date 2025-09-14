ad4181d93cb817b9ba06063719aab7a37b27c307
AlvaroPerezUTRGV <alvaro.perez01@utrgv.edu>
2025-09-14 22:35:41 +0000

    Finished-Added Messages

--- FunctionsLab.cpp
+++ FunctionsLab.cpp
@@ -12,13 +12,13 @@ using namespace std; // making life easier so that we do not need to use std::ci
 
 int locateSmallest(int array[], int start, int end){
 	
-	int Smallest_Index = start ;
-	for(int i = start; i <= end; i++){
-		if (array[i]< array[Smallest_Index]){
-			Smallest_Index = i;
+	int Smallest_Index = start ; //initilized a variable that would hold the smallest value. Not the actual value but the index
+	for(int i = start; i <= end; i++){      // a simple for loop that would look through the array given a parameter
+		if (array[i]< array[Smallest_Index]){  // an if statement that that if the value of my array is less than my previous smallest it will be replaced as the smallest index
+			Smallest_Index = i;  // updated the smallest index value
 		}
 	}
-	return Smallest_Index; // fix this!
+	return Smallest_Index; // returns the Smallest_Index
 }
 
 //Write the function called replaceVariable in the given interval such that 
@@ -29,9 +29,9 @@ int locateSmallest(int array[], int start, int end){
 // function: Search in the array from given start to end index and replace the given target value with 1000
 void replaceVariable(int array[], int target, int start, int end) {
 
-	for(int i = start; i<= end; i++){
-		if (array[i] == target) {
-			array[i] = 1000;
+	for(int i = start; i<= end; i++){ //a simple for loop to go through my array given a parameter
+		if (array[i] == target) { //if statement that says if the value of my array equals to my target number then the next line will be executed
+			array[i] = 1000; //replaces the element of the array to 1000
 		}
 	}
 }
@@ -42,10 +42,10 @@ void replaceVariable(int array[], int target, int start, int end) {
 //output: nothing
 //input: the array of integers and its size
 int printArray(int array[], int size) {
-	size = sizeof(array) / sizeof(array[0]);
-	cout << "My array elements are: " ;
-	for(int i = 0; i < size; ++i){
-		cout << array[i] << " ";
+	size = sizeof(array) / sizeof(array[0]); //got the size of my array
+	cout << "My array elements are: " ; //simple output of a string
+	for(int i = 0; i < size; ++i){ // a for loop to that will execute the next line of code by the size of my array
+		cout << array[i] << " "; // prints out the array
 	}
 	cout << endl;
 	
@@ -68,11 +68,11 @@ int printArray(int array[], int size) {
 
 
 int sumOdds(int array[], int start, int end){
-	int sum = 0;
-	for (int i = start; i <= end; i++){
-		if (array[i] % 2 != 0){
-			sum = sum + array[i];
+	int sum = 0; // initilize a variable that will hold the sum of all my odd numbers
+	for (int i = start; i <= end; i++){ // a for loop that will go through the array given a parameter 
+		if (array[i] % 2 != 0){ // if statement to find the odd numbers by diving by 2
+			sum = sum + array[i]; // adds the odd numbers together and saves them to my variable sum 
 		}
 	}
-	return sum;
+	return sum; //returns to sum
 }
